45bfeddbb870c796f3240e35341f5b594af7eb64
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-03-08 12:03:00 +0500

    extension for UIView is added

--- FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Extensions/UIView+Extension.swift
+++ FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Extensions/UIView+Extension.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 extension UIView{
+    func round(_ radius: CGFloat = 10){
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+    }
     
+    func addBorder(color: UIColor, width: CGFloat){
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = width
+    }
 }
