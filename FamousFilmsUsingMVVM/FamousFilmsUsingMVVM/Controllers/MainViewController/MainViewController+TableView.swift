70536e502ca2b11887e36d36a15f2af4799ec725
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-03-08 12:02:42 +0500

    population and navigation in table for main screen

--- FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Controllers/MainViewController/MainViewController+TableView.swift
+++ FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Controllers/MainViewController/MainViewController+TableView.swift
@@ -18,7 +18,7 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func registerCells(){
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(MainMovieCell.register(), forCellReuseIdentifier: MainMovieCell.identifier)
     }
     
     func reloadTableView(){
@@ -36,9 +36,19 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = viewModel.getMovieName(in: indexPath.row)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MainMovieCell.identifier, for: indexPath) as? MainMovieCell else { return UITableViewCell() }
+        let cellViewModel = cellDataSource[indexPath.row]
+        cell.setUpCell(viewModel: cellViewModel)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let movieId = cellDataSource[indexPath.row].id
+        self.openDetails(movieId: movieId)
     }
     
 }
