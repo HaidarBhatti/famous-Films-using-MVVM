d600cb505aaa54ef8c212ab7a5d8dc6749f748e6
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-03-09 12:56:11 +0500

    getPopularMovies method is added in APIServices

--- FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Networking/APIServices.swift
+++ FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Networking/APIServices.swift
@@ -34,4 +34,25 @@ class APIServices{
             }
         }.resume()
     }
+    
+    static func getPopularMovies(language: OriginalLanguage, page: Int, completionHandler: @escaping (_ result: Result<PopularMoviesModel,NetworkError>) -> Void ){
+        
+        let instance = NetworkConstant.shared
+        let stringURL = instance.serverAddress+instance.movie+"/\(instance.popular)?"+"\(instance.apiStringWithKey)&\(instance.language)=\(language.rawValue)&\(instance.page)=\(page)"
+        
+        guard let url = URL(string: stringURL) else {
+            completionHandler(.failure(.urlError))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
+            if error == nil, let data = dataResponse, let resultData = try? JSONDecoder().decode(PopularMoviesModel.self, from: data){
+                completionHandler(.success(resultData))
+            }
+            else{
+                completionHandler(.failure(.canNotParseData))
+            }
+        }.resume()
+    }
+        
 }
