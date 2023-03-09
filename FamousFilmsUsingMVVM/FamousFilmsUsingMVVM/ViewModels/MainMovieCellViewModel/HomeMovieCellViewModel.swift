2465e978fd938a7767ef667b5bdb3bb688c88db7
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-03-09 12:58:09 +0500

    MainMovieCellViewModel is added

---
+++ FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/ViewModels/MainMovieCellViewModel/HomeMovieCellViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  yuio.swift
+//  FamousFilmsUsingMVVM
+//
+//  Created by Haidar Bhatti on 08/03/2023.
+//
+
+import Foundation
+
+
+class MainMovieCellViewModel{
+            
+    var id: Int
+    var movieTitle: String
+    var movieReleaseDate: String
+    var popularity: Double
+    var movieImageURL: URL?
+    
+    init(movie: TrendingMovies){
+        id = movie.id
+        movieTitle = movie.name ?? movie.title ?? ""
+        movieReleaseDate = movie.releaseDate ?? movie.firstAirDate ?? ""
+        popularity = movie.popularity
+        movieImageURL = makeImageURL(imageLink: movie.posterPath)
+    }
+    
+    func makeImageURL(imageLink: String) -> URL?{
+        return URL(string: "\(NetworkConstant.shared.imageServerAddress)\(imageLink)")
+    }
+    
+}
