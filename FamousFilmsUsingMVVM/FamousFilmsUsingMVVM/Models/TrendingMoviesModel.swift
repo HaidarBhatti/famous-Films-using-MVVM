decb4e8c9b827ffa103155e449ef66bad64804c9
haidar bhatti <prominute@onyxtec.co>
2023-03-17 23:08:44 +0500

    pod added for segmented controll

--- FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Models/TrendingMoviesModel.swift
+++ FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Models/TrendingMoviesModel.swift
@@ -7,79 +7,6 @@
 
 import Foundation
 
-//struct TrendingMoviesModel: Codable {
-//    let page: Int
-//    let results: [TrendingMovies]
-//    let totalPages, totalResults: Int
-//
-//    enum CodingKeys: String, CodingKey {
-//        case page, results
-//        case totalPages = "total_pages"
-//        case totalResults = "total_results"
-//    }
-//}
-//
-//// MARK: - Result
-//struct TrendingMovies: Codable {
-//    let adult: Bool
-//    let backdropPath: String
-//    let id: Int
-//    let title: String?
-//    let originalLanguage: OriginalLanguage
-//    let originalTitle: String?
-//    let overview, posterPath: String
-//    let mediaType: MediaType
-//    let genreIDS: [Int]
-//    let popularity: Double
-//    let releaseDate: String?
-//    let video: Bool?
-//    let voteAverage: Double
-//    let voteCount: Int
-//    let name, originalName, firstAirDate: String?
-//    let originCountry: [String]?
-//
-//    enum CodingKeys: String, CodingKey {
-//        case adult
-//        case backdropPath = "backdrop_path"
-//        case id, title
-//        case originalLanguage = "original_language"
-//        case originalTitle = "original_title"
-//        case overview
-//        case posterPath = "poster_path"
-//        case mediaType = "media_type"
-//        case genreIDS = "genre_ids"
-//        case popularity
-//        case releaseDate = "release_date"
-//        case video
-//        case voteAverage = "vote_average"
-//        case voteCount = "vote_count"
-//        case name
-//        case originalName = "original_name"
-//        case firstAirDate = "first_air_date"
-//        case originCountry = "origin_country"
-//    }
-//}
-//
-//enum MediaType: String, Codable {
-//    case movie = "movie"
-//    case tv = "tv"
-//    case all = "all"
-//    case person = "person"
-//}
-//
-//enum OriginalLanguage: String, Codable {
-//    case de = "de"
-//    case en = "en"
-//    case es = "es"
-//    case ja = "ja"
-//}
-//
-//enum TimeWindow: String, Codable{
-//    case day = "day"
-//    case week = "week"
-//}
-
-
 struct TrendingMoviesModel: Codable {
     let page: Int
     let results: [TrendingMovies]
@@ -97,39 +24,32 @@ struct TrendingMovies: Codable {
     let adult: Bool
     let backdropPath: String
     let id: Int
-    let name: String?
+    let title: String
     let originalLanguage: OriginalLanguage
-    let originalName: String?
-    let overview, posterPath: String
+    let originalTitle, overview, posterPath: String
     let mediaType: MediaType
     let genreIDS: [Int]
     let popularity: Double
-    let firstAirDate: String?
+    let releaseDate: String
+    let video: Bool
     let voteAverage: Double
     let voteCount: Int
-    let originCountry: [String]?
-    let title, originalTitle, releaseDate: String?
-    let video: Bool?
-
+    
     enum CodingKeys: String, CodingKey {
         case adult
         case backdropPath = "backdrop_path"
-        case id, name
+        case id, title
         case originalLanguage = "original_language"
-        case originalName = "original_name"
+        case originalTitle = "original_title"
         case overview
         case posterPath = "poster_path"
         case mediaType = "media_type"
         case genreIDS = "genre_ids"
         case popularity
-        case firstAirDate = "first_air_date"
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
-        case originCountry = "origin_country"
-        case title
-        case originalTitle = "original_title"
         case releaseDate = "release_date"
         case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
     }
 }
 
@@ -146,6 +66,9 @@ enum OriginalLanguage: String, Codable {
     case ja = "ja"
     case ko = "ko"
     case es = "es"
+    case fr = "fr"
+    case it = "it"
+    case nl = "nl"
 }
 
 enum TimeWindow: String, Codable{
