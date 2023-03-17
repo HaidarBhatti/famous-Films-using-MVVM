6fbe6ce64019476163a995cc9238dd2113ac6a28
haidar bhatti <prominute@onyxtec.co>
2023-03-17 23:10:17 +0500

    TrendingMovieDayModel is added

---
+++ FamousFilmsUsingMVVM/FamousFilmsUsingMVVM/Models/TrendingMovieDayModel.swift
@@ -0,0 +1,54 @@
+//
+//  TrendingMovieDayModel.swift
+//  FamousFilmsUsingMVVM
+//
+//  Created by Haidar Bhatti on 17/03/2023.
+//
+
+import Foundation
+
+struct TrendingMovieDayModel: Codable {
+    let page: Int
+    let results: [MovieDayModel]
+    let totalPages, totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct MovieDayModel: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let id: Int
+    let title: String
+    let originalLanguage: OriginalLanguage
+    let originalTitle, overview, posterPath: String
+    let mediaType: MediaType
+    let genreIDS: [Int]
+    let popularity: Double
+    let releaseDate: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case id, title
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case mediaType = "media_type"
+        case genreIDS = "genre_ids"
+        case popularity
+        case releaseDate = "release_date"
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
